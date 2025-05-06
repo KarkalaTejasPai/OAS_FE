86610459065fee4b235a2cddf753a807d81a942d
Arshjeet Singh <2393199@cognizant.com>
2025-05-06 10:33:50 +0530

    Navbar Flow

--- src/app/sell-product/sell-product.component.ts
+++ src/app/sell-product/sell-product.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/shared/header/header.component';
+import { FooterComponent } from '../components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-sell-product',
-  imports: [],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './sell-product.component.html',
   styleUrl: './sell-product.component.css'
 })
